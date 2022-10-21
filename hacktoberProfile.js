aa03051a851f5cae10168dc4ff37635f9d167ac5
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2022-10-21 16:55:21 +0530

    Added swayamterode
    Indented the Code and fixed some syntax error like missing of ','

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -20,31 +20,26 @@ var profiles = [
         place: 'Hyderabad,Telangana,India',
         github: 'shreyamalogi',
     },
-
     {
         name: 'Skand Sharma',
         place: 'Jamshedpur, Jharkhand, India',
         github: 'skand1110',
     },
-
     {
         name: 'Prakash kumar',
         place: 'Patna,Bihar,India',
         github: 'prakash-sah-lab',
     },
-
     {
         name: 'Manish Saha',
         place: 'Kolkata, West Bengal, India',
         github: 'Manice18',
     },
-
     {
         name: 'Shruthi Kotawar',
         place: 'Hyderabad,Telangana,India',
         github: 'Shruthi-kotawar',
     },
-
     {
         name: 'Niranjan Kurhade',
         place: 'Mumbai,Maharashtra,India',
@@ -55,67 +50,56 @@ var profiles = [
         place: 'Sagar,Madhya Pradesh,India',
         github: 'SumitRaikwar18'
     },
-
     {
         name: 'Miswar Yousuf',
         place: 'Hyderabad,Telangana,India',
         github: 'miswaryousuft',
     },
-
     {
         name: 'Pratyusha Maisa',
         place: 'Hyderabad,Telangana,India',
         github: 'Pratyusha-spec',
     },
-
     {
         name: 'Sankooru Manideep',
         place: 'Hyderabad, Telangana, India',
         github: 'sankooru14',
     },
-
     {
         name: 'Yorgina Lecaros',
         place: 'Lima, Peru',
         github: 'YorLecaros',
     },
-
     {
         name: 'Harshit Pachori',
         place: 'Aligarh, UttarPradesh, India',
         github: 'harshitpachori',
     },
-
     {
         name: 'Amit Das',
         place: 'Kolkata, India',
         github: 'das-amit',
     },
-
     {
         name: 'Nikhil Raikwar',
         place: 'Sagar,Madhya Pradesh,India',
         github: 'NikhilRaikwar',
     },
-
     {
         name: 'Akasaka Ryuunosuke',
         place: 'Tokyo, Japan',
         github: 'idm1303',
     },
-
     {
         name: 'Shravya Malogi',
         place: 'Hyderabad,Telangana,India',
         github: 'shravyamalogi',
     },
-
     {
         name: 'Noundla Chandrasangna',
         place: 'Karimnagar,Telangana,India',
         github: 'chandrasangna',
     },
-
     {
         name: 'Shivani Thudimilla',
         place: 'Suryapet,Telangana,India',
@@ -126,56 +110,46 @@ var profiles = [
         place: 'Jangoan, Telangana, India',
         github: 'niharikaponugoti',
     },
-
     {
         name: 'Rio Cantre',
         place: 'Italy',
         github: 'RioCantre',
     },
-
     {
         name: 'Savio Dias',
         place: 'Mumbai,Maharashtra,India',
         github: 'Savio629',
     },
-
     {
         name: "Jesse Mayo",
         place: "Raleigh, NC, USA",
         github: "jsmayo"
     },
-
     {
         name: "Amitabh Dasgupta",
         place: "Kolkata, West Bengal, India",
         github: "Shelby-kun"
     },
-
     {
         name: "Ankit Goyal",
         place: "Sirsa, Haryana, India",
         github: "Ankit9126"
     },
-
     {
         name: "Koustav Saha",
         place: "Kolkata, West Bengal, India",
         github: "k0ustav"
     },
-
     {
         name: "Himanshu Saha",
         place: "Purnea, Bihar, India",
         github: "Himanshu-Saha"
     },
-
-
     {
         name: "Shivam Jain",
         place: "Chandigarh, India",
         github: "Shiv-jain"
     },
-
     {
         name: "Ankitha jalagam",
         place: "Suryapet,India",
@@ -206,7 +180,7 @@ var profiles = [
         github: "IbrahimShareeff"
     },
     {
-        name:"meneni spandana"
+        name:"meneni spandana",
         place:"suryapet, Telangana, India",
         github:"Menenispandana"
      },
@@ -220,51 +194,30 @@ var profiles = [
         place: 'Rajahmundry,Anshra pradesh, India',
         github: 'eswaritammana',
     },
-    
     {
         name: 'Siddhant Srivastava',
         place: 'Purnea, Bihar, India',
         github: 'sidsrivastavasks',
     },
-
     {
     name : "Vaibhav Radhakrishnan",
     place : "Hyderabad,Telengana,India",
     github : "Vai300"
-  }
-    
+    },
     {   
      name: 'Rohit Gupta',
         place: 'Hyderabad,Telangana,India',
         github: 'RoHitHai',
     },
-
     {
         name: "Ruthvik Kanukunta",
         place: "Hyderabad, Telangana, India",
         github: "Ruthvik2127"
     },
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    {
+        name: "Swayam Terode",
+        place: "Chandrapur, Maharashtra, India",
+        github: "swayamterode"
+    },
     //end of json
 ];
